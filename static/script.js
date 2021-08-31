c48a4570ab34482963ac7fedbe33a38b3169fb8b
Anil Tarar <aniltarar@gmail.com>
2021-08-31 14:28:21 +1000

    Main looks are ready. A few CSS touch ups needed.

---
+++ static/script.js
@@ -0,0 +1,58 @@
+
+// let degrees = 0;
+// function rotateAnimation(id,speed){
+//   wheel = document.getElementById(id)
+
+//   degrees++;
+// 	if(navigator.userAgent.match("Chrome")){
+// 		wheel.style.WebkitTransform = `rotate(${degrees}deg)`;
+// 	} else if(navigator.userAgent.match("Firefox")){
+// 		wheel.style.MozTransform = `rotate(${degrees}deg)`;
+// 	} else if(navigator.userAgent.match("MSIE")){
+// 		wheel.style.msTransform = `rotate(${degrees}deg)`;
+// 	} else if(navigator.userAgent.match("Opera")){
+// 		wheel.style.OTransform = `rotate(${degrees}deg)`;
+// 	} else {
+// 		wheel.style.transform = `rotate(${degrees}deg)`;
+// 	}
+//   setTimeout(`rotateAnimation('${id}' , ${speed})`, speed);
+//   console.log(speed)
+// 	if(degrees > 359){
+// 		degrees = 1;
+// 	}
+
+// };
+
+// rotateAnimation('img1',30)
+
+// var vel = 2000;
+// var degs = 0;
+// var cat = document.querySelector("img");
+// function repeat(){
+//   if(vel == 1000){
+//     cat.classList.add("hover");
+//     console.log("hover")
+//   }else{
+//     cat.classList.remove("hover");
+//     console.log("nohover")
+//   }
+//   degs+=360;
+//   cat.style.transform = "rotate("+degs+"deg)";
+//   setTimeout(repeat, vel);
+// }
+// repeat();
+// document.querySelector("img").addEventListener("mouseenter",hovering);
+// function hovering(){
+//   vel = 1000;
+// }
+// document.querySelector("img").addEventListener("mouseleave",nohovering);
+// function nohovering(){
+//   vel = 2000;
+// }
+
+// const linkedIn = document.getElementById("linkedIn")
+
+// linkedIn.addEventListener("click" , (e)=>{
+    
+// })
+
